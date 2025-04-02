6c6e2563e5f3e4f3e2abb44c926c263e49a6c17c
Mohammadreza Zakeri (Zaki) <31301756+ZAKI1905@users.noreply.github.com>
2025-04-02 13:24:35 -0400

    Update mag_induce.py

--- mag_induce.py
+++ mag_induce.py
@@ -15,6 +15,7 @@ if page == "Background Information":
     
     st.header("Magnetic Field Formulas and Explanations")
     st.markdown(r"""
+    
     **Vacuum Permeability ($\mu_0$)** 
     The vacuum permeability ($\mu_0$) characterizes the ability of free space to support a magnetic field. Its standard value is:""")
 
